f081ce75a5d3c27596de949101cf16ee955969d2
wellingtonumbelino <wellington.s.umbelino@gmail.com>
2023-07-02 11:49:52 -0300

    Creating project and creating firts page of the system.

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Login from './pages/Login/Login.vue';
+</script>
+
+<template>
+  <Login />
+</template>
---
+++ src/pages/Login/Login.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="login">
+		<div class="login-container">
+			<div class="welcome-message">
+				<h1>Welcome Back!</h1>
+			</div>
+			<div class="login-actions">
+				<h1>Login</h1>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref("Hello World");
+</script>
+
+<style lang="scss">
+@import './login-style.scss'
+</style>
